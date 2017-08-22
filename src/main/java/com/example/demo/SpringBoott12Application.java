3bd1b444430fbd8637c94f36ab553586b7c043eb
RVSW317028DT <RVSW317028DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-22 14:45:51 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoott12Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoott12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoott12Application.class, args);
+	}
+}
